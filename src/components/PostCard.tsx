99970579e4289aa41801736e6a79e10a1f907ff8
XKMai <xkmai014@gmail.com>
2024-12-29 11:09:52 +0800

    Added button to edit/delete posts

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,8 @@
+import { Button, CardActions, Menu, MenuItem } from "@mui/material";
 import Card, { CardProps } from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 interface Props {
   name: string;
@@ -8,12 +10,30 @@ interface Props {
 }
 
 const PostCard: React.FC<Props & CardProps> = ({ name, text, ...props }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card sx={{ width: "100%" }} {...props}>
       <CardContent>
         <Typography variant="h5">{name}</Typography>
         <Typography variant="body1">{text}</Typography>
       </CardContent>
+      <CardActions>
+        <Button size="small" onClick={handleClick}>
+          Options
+        </Button>
+        <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>Edit</MenuItem>
+          <MenuItem onClick={handleClose}>Delete</MenuItem>
+        </Menu>
+      </CardActions>
     </Card>
   );
 };
