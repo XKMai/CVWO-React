15237639af07b92c7b368f23207a00fcc3d4fcfa
XKMai <xkmai014@gmail.com>
2025-01-13 13:04:42 +0800

    Router setup in main

--- CVWO-Backend/cmd/server/main.go
+++ CVWO-Backend/cmd/server/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	//"net/http"
+
+	//"fmt"
+
 	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/database"
+	//"github.com/XKMai/CVWO-React/CVWO-Backend/internal/models"
+	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/router"
+	//"github.com/XKMai/CVWO-React/CVWO-Backend/internal/handlers/users"
+	//"gorm.io/driver/postgres"
+	//"gorm.io/gorm"
 )
 
 func main() {
-    database.SetupDatabase()
+    router.Setup()
+	database.SetupDatabase()
+	//db, err := gorm.Open(postgres.Open("cvwo_db"), &gorm.Config{})
+	//user := models.User{ID: 1, Role:"User", Name: "Xin Kai", Password: "password"}
 }
