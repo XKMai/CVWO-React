f49b54337b1f28b97789b29f791947a7b8352f64
XKMai <xkmai014@gmail.com>
2025-01-13 15:24:35 +0800

    Fixed router to use middleware first

--- CVWO-Backend/internal/router/router.go
+++ CVWO-Backend/internal/router/router.go
@@ -1,20 +1,41 @@
 package router
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/routes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/gorm"
 )
 
-func Setup() chi.Router {
+var DB *gorm.DB
+
+func Setup(db *gorm.DB) chi.Router {
+	DB = db
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(MyMiddleware)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("OK"))
     })
 	r.Mount("/users", routes.UserRoutes())
-	http.ListenAndServe(":3000",r)
 	return r
 }
+
+func MyMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	  // create new context from `r` request context, and assign key `"user"`
+	  // to value of `"123"`
+	  ctx := context.WithValue(r.Context(), "db", DB)
+  
+	  // call the next handler in the chain, passing the response writer and
+	  // the updated request object with the new context value.
+	  //
+	  // note: context.Context values are nested, so any previously set
+	  // values will be accessible as well, and the new `"user"` key
+	  // will be accessible from this point forward.
+	  next.ServeHTTP(w, r.WithContext(ctx))
+	})
+  }
