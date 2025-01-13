a636bdd825dccad00c3dde10428318ed7d867889
XKMai <xkmai014@gmail.com>
2025-01-13 13:03:36 +0800

    Temporary router and routes

--- CVWO-Backend/internal/router/router.go
+++ CVWO-Backend/internal/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/routes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -13,6 +14,7 @@ func Setup() chi.Router {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("OK"))
     })
+	r.Mount("/users", routes.UserRoutes())
 	http.ListenAndServe(":3000",r)
 	return r
 }
--- CVWO-Backend/internal/routes/routes.go
+++ CVWO-Backend/internal/routes/routes.go
@@ -1,12 +1,11 @@
 package routes
 
 import (
-	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/handlers/posts"
 	"github.com/XKMai/CVWO-React/CVWO-Backend/internal/handlers/users"
 	"github.com/go-chi/chi/v5"
 )
 
-func UserRoutes() func(chi.Router) {
+func UserRoutes() (chi.Router) {
 	r := chi.NewRouter()
 	userhandler := users.UserHandler{}
 	r.Get("/", userhandler.ListUsers)
@@ -14,20 +13,15 @@ func UserRoutes() func(chi.Router) {
 	r.Get("/{id}", userhandler.GetUser)
 	r.Put("/{id}", userhandler.UpdateUser)
 	r.Delete("/{id}", userhandler.DeleteUser)
-	return func(r chi.Router) {
-		r.Mount("/", r)
-	}
+	return r
 }
 
-func PostRoutes() func(chi.Router) {
-	r := chi.NewRouter()
-	userhandler := posts.PostHandler{}
-	r.Get("/", userhandler.ListPosts)
-	r.Post("/", userhandler.CreatePost)
-	r.Get("/{id}", userhandler.GetPost)
-	r.Put("/{id}", userhandler.UpdatePost)
-	r.Delete("/{id}", userhandler.DeletePost)
-	return func(r chi.Router) {
-		r.Mount("/", r)
-	}
-}
+// func PostRoutes() (chi.Router) {
+// 	r := chi.NewRouter()
+// 	userhandler := posts.PostHandler{}
+// 	r.Get("/", userhandler.ListPosts)
+// 	r.Post("/", userhandler.CreatePost)
+// 	r.Get("/{id}", userhandler.GetPost)
+// 	r.Put("/{id}", userhandler.UpdatePost)
+// 	r.Delete("/{id}", userhandler.DeletePost)
+// }
