f7f1d25dab9b5248552c7e063c946e3eb2beaf77
XKMai <xkmai014@gmail.com>
2025-01-13 13:02:29 +0800

    Updated Models

--- CVWO-Backend/internal/models/comment.go
+++ CVWO-Backend/internal/models/comment.go
@@ -4,13 +4,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type Post struct {
+type Comment struct {
 	gorm.Model
 	ID       uint   `json:"id" gorm:"primaryKey"`
-	Title    string `json:"title"`
 	Content  string `json:"content"`
-	Category string `json:"category"` //Can have multiple categories
 	User    User   `json:"owner"`
+	Post	Post   `json:"post"`
 	Datetime string `json:"date_time"`
 	Score    uint   `json:"score"`
 }
--- CVWO-Backend/internal/models/user.go
+++ CVWO-Backend/internal/models/user.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
 	ID       uint   `json:"id" gorm:"primaryKey"`
 	Role     string `json:"role"`
 	Name     string `json:"name"`
