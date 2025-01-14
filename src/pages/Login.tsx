d884128d0a33ddc8dda560d1f4227555eede142a
XKMai <xkmai014@gmail.com>
2025-01-14 20:39:34 +0800

    Added Header, side Picture and Login fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,10 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
+import header from "../components/Header";
+import { AspectRatio, Margin, Padding } from "@mui/icons-material";
+import SignUp from "../components/SignUp";
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -11,16 +14,53 @@ const Item = styled(Paper)(({ theme }) => ({
 function Login() {
   return (
     <>
-      <Box position={"absolute"} top={"40%"} left={"60%"}>
-        <Grid container spacing={2} columns={12}>
-          <Grid size={12}>
-            <TextField label="Username" variant="outlined" />
+      {header()}
+      <Grid
+        container
+        direction={"row"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Box height="90vh" width="50%" overflow={"hidden"}>
+          <img src="public\Mountain.webp" style={{ height: "100%" }} />
+        </Box>
+        <Box height="90vh" width="50%">
+          <Grid
+            container
+            height={"100%"}
+            direction={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Grid style={{ width: "30%" }} sx={{ mb: 2 }}>
+              <Typography align="left" fontSize={34}>
+                Login
+              </Typography>
+            </Grid>
+            <Grid style={{ width: "30%" }} sx={{ mb: 2 }}>
+              <TextField label="Username" variant="outlined" fullWidth />
+            </Grid>
+            <Grid style={{ width: "30%" }} sx={{ mb: 0 }}>
+              <TextField label="Password" variant="outlined" fullWidth />
+            </Grid>
+            <Grid
+              style={{ width: "30%" }}
+              sx={{ mb: 2 }}
+              justifyContent={"space-between"}
+              container
+              alignItems={"center"}
+            >
+              {SignUp()}
+              <Button>Forget Password?</Button>
+            </Grid>
+            <Grid style={{ width: "30%" }}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
           </Grid>
-          <Grid>
-            <TextField label="Password" variant="outlined" />
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Grid>
     </>
   );
 }
