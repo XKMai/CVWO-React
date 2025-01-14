dfea6672cee1260edd8b24a03ded6a9d245c9801
XKMai <xkmai014@gmail.com>
2025-01-14 20:39:00 +0800

    Button for Signing up on Login page. Uses a Dialog

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import React from "react";
+
+export default function SignUp() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleClickOpen}>Sign Up</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Sign Up</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter your Username and Password here to sign up.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="name"
+            label="Name"
+            type="name"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
