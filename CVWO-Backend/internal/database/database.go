2e36aa9b395a0dc67dfc7174ad61741fe238c36c
XKMai <xkmai014@gmail.com>
2025-01-23 22:24:41 +0800

    Remove extra code

--- CVWO-Backend/internal/database/database.go
+++ CVWO-Backend/internal/database/database.go
@@ -15,14 +15,10 @@ var db *gorm.DB
 func SetupDatabase() *gorm.DB {
 	dsn := "host=localhost user=postgres password=Abc123!@# dbname=cvwo_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	  if err != nil {
+	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 	fmt.Println("Database connection established!")	
-	db.AutoMigrate(&models.User{}, &models.Post{})
+	db.AutoMigrate(&models.User{}, &models.Post{},&models.Comment{})
 	return db
-}
-
-func GetUser(name string) string {
-	return "test"
 }
