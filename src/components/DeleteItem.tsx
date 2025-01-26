bbb5d23e9c99d35e99aa74e657a0bc330b12c462
XKMai <xkmai014@gmail.com>
2025-01-26 23:51:01 +0800

    Fixed Delete Item

--- src/components/DeleteItem.tsx
+++ src/components/DeleteItem.tsx
@@ -1,6 +1,4 @@
-import { IconButton } from "@mui/material";
 import { Post } from "../types/Post";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import axios from "axios";
 import { Comment } from "../types/Comment";
 
@@ -12,21 +10,10 @@ function isPost(item: Post | Comment): item is Post {
   return (item as Post).title !== undefined;
 }
 
-const DeleteItem: React.FC<Props> = ({ item }) => {
-  const handleClick = () => {
-    if (isPost(item)) {
-      axios.delete(`/api/protected/posts/${item.ID}`);
-    } else {
-      axios.delete(`/api/protected/comments/${item.ID}`);
-    }
-  };
-  return (
-    <>
-      <IconButton onClick={handleClick}>
-        <MoreVertIcon />
-      </IconButton>
-    </>
-  );
-};
-
-export default DeleteItem;
+export default function DeleteItem(item) {
+  if (isPost(item)) {
+    axios.delete(`/api/protected/posts/${item.ID}`);
+  } else {
+    axios.delete(`/api/protected/comments/${item.ID}`);
+  }
+}
